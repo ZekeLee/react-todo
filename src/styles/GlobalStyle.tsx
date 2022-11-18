50b762a913ed72aa1790bda0024af937ee969c9d
web.ljg <web.ljg@gmail.com>
2022-11-18 22:29:35 +0900

    소스 코드 정리, 공통 스타일 정의

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  *, *:before, *:after{box-sizing:border-box;}
+  
+  html{font-size:16px;}
+  html,body{height:100%;}
+  body{
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    font-weight: 400;
+    color: #fff;
+    word-break:break-word;
+    background-color: #000;
+  }
+  #root{
+    margin: 0 auto;
+    height: 100%;
+    overflow: hidden auto;
+  }
+  a, button, input{all: unset;}
+  a, button{cursor:pointer;}
+`;
+
+export default GlobalStyle;
